23ebd458e7894165306d15983c0895355cad3122
Thái Việt Anh Khoa <khoa.thai2411655@hcmut.edu.vn>
2026-07-01 22:12:39 +0700

    week02: hoan thanh bai tap buoi 2

--- week02/buoi02.thaivietanhkhoa.c
+++ week02/buoi02.thaivietanhkhoa.c
@@ -8,15 +8,12 @@
 typedef union {
     uint16_t raw_value;
     struct {
-        // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
-
-
-
-
-        // HỌC VIÊN KẾT THÚC VIẾT CODE
+        uint16_t PWR_ON : 1;
+        uint16_t ASSIST_LEVEL : 2;
+        uint16_t LIGHT_BRIGHT : 4;
+        uint16_t RESERVED : 9;
     } fields;
 } Bike_Status_t;
-
 
 // TASK 2: FUNCTION POINTERS & TABLE-DRIVEN ARCHITECTURE
 
@@ -30,25 +27,16 @@ void drive_sport(void) {
     printf("Bike Mode: SPORT.\n");
 }
 
-// HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
-
-
-
-
-// HỌC VIÊN KẾT THÚC VIẾT CODE
-
+void (*drive_modes[3])(void) = {drive_eco, drive_normal, drive_sport};
 
 // TASK 3: ARCHITECTURE CALLBACKS
 
 void Battery_Monitor(void (*overheat_cb)(void)) {
     int battery_temp = 45; 
-    
-    // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
-
-
-
-
-    // HỌC VIÊN KẾT THÚC VIẾT CODE
+    if (battery_temp > 40) {
+        if (overheat_cb != 0)
+        overheat_cb();
+    }
 }
 
 void Critical_Battery_Handler(void) {
@@ -62,12 +50,9 @@ const char BIKE_MODEL[] = "E-Bike X2026";
 uint32_t total_odometer = 0;             
 
 void crash_simulation(void) {
-    // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
-
-
-
-
-    // HỌC VIÊN KẾT THÚC VIẾT CODE
+    volatile char dummy_buffer[256];
+    dummy_buffer[0] = 1;
+    crash_simulation();
 }
 
 
@@ -84,12 +69,11 @@ int main() {
 
     // 2. Test Task 2
     printf("ENGINE CONTROLLING: \n");
-    // HỌC VIÊN BẮT ĐẦU VIẾT CODE TỪ ĐÂY
+    uint8_t mode = my_bike.fields.ASSIST_LEVEL;
+    uint8_t safe_index = (mode < 3) ? mode : 0;
+    drive_modes[safe_index]();
+    printf("\n");
 
-
-
-
-    // HỌC VIÊN KẾT THÚC VIẾT CODE
 
     // 3. Test Task 3
     Battery_Monitor(Critical_Battery_Handler);
@@ -103,7 +87,7 @@ int main() {
     printf("current_speed (RAM/Stack):  %p\n", (void*)&current_speed);
 
     // Bỏ comment dòng dưới để chạy thử bài Crash Lab
-    // crash_simulation();
+    crash_simulation();
 
     return 0;
 }
